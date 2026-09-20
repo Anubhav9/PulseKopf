bdd64bcaa43008d0ed31d330316ffcb40d274cdd
Anubhav Sanyal <princebest3@rediffmail.com>
2026-09-20 12:08:59 +0530

    Corrected the file

--- controller/kopf_controller.py
+++ controller/kopf_controller.py
@@ -1,40 +1,71 @@
-from models import metrics_model
-import yaml
+import kopf
+from collector import metrics_collector
+from datetime import datetime, timezone
+from observability import prometheus_metrics
 
-def return_spec_yaml():
-    schema=metrics_model.MetricsModel.model_json_schema()
-    yaml_schema = yaml.safe_dump(
-        schema,
-        sort_keys=False
-    )
-    return yaml_schema
-
-def return_spec_status_yaml():
-    schema=metrics_model.MetricsModelStatus.model_json_schema()
-    yaml_schema = yaml.safe_dump(
-        schema,
-        sort_keys=False
-    )
-    return yaml_schema
-
-def return_complete_crd():
-    spec_yaml=return_spec_yaml()
-    file_path = "resources/health_metrics_crd.yaml"
-
-    with open(file_path, "r") as file:
-        crd = yaml.safe_load(file)
+@kopf.on.create("healthmetrics.anubhavdev.com","v1alpha1","healthmetrics")
+def resource_creation(namespace,**kwargs):
+    kopf.info(f"Resource created in namespace: {namespace}")
 
 
-    crd["spec"]["versions"][0]["schema"] = {
-        "openAPIV3Schema": {
-            "type": "object",
-            "properties": {
-                "spec": spec_yaml
-            }
-        }
-    }
-    crd["spec"]["versions"][0]["schema"]["openAPIV3Schema"]["properties"]["spec"] = yaml.safe_load(spec_yaml)
+@kopf.on.update("healthmetrics.anubhavdev.com","v1alpha1","healthmetrics")
+def resource_updation(spec, namespace, **kwargs):
+    kopf.info(f"Resource updated in namespace: {namespace} with spec: {spec}")
+    heartRateSpec=spec["heartRate_enabled"]
+    stepCountSpec=spec["stepCount_enabled"]
+    activeEnergySpec=spec["activeEnergy_enabled"]
+    restingEnergySpec=spec["restingEnergy_enabled"]
 
-    with open(file_path, "w") as file:
-        yaml.safe_dump(crd, file, sort_keys=False)
+    if heartRateSpec:
+        kopf.info(f"Heart Rate metric collection is enabled in namespace: {namespace}")
+    if stepCountSpec:
+        kopf.info(f"Step Count metric collection is enabled in namespace: {namespace}")
+    if activeEnergySpec:
+        kopf.info(f"Active Energy metric collection is enabled in namespace: {namespace}")
+    if restingEnergySpec:
+        kopf.info(f"Resting Energy metric collection is enabled in namespace: {namespace}")
+
+@kopf.timer("healthmetrics.anubhavdev.com","v1alpha1","healthmetrics",interval=60.0)
+def collect_metrics(spec,status,patch,namespace,**kwargs):
+    kopf.info(f"Collecting metrics for resource in namespace: {namespace}")
+    heartRateSpec=spec["heartRate_enabled"]
+    stepCountSpec=spec["stepCount_enabled"]
+    activeEnergySpec=spec["activeEnergy_enabled"]
+    restingEnergySpec=spec["restingEnergy_enabled"]
+    current_timestamp = datetime.now(timezone.utc).replace(microsecond=0).isoformat()
+    if heartRateSpec:
+        heartRateData=metrics_collector.collect_metrics_after_interval("heartRate",spec["interval"])
+        patch.status["current_heartRate"]=heartRateData.get("value","Not Enabled")
+        patch.status["lastupdated_timestamp"]=str(current_timestamp)
+        prometheus_metrics.heartRate.set(heartRateData.get("value",0))
+    elif not heartRateSpec:
+        patch.status["current_heartRate"]="Not Enabled"
+        patch.status["lastupdated_timestamp"]="Not Enabled"
+
+    if stepCountSpec:
+        stepCountData=metrics_collector.collect_metrics_after_interval("stepCount",spec["interval"])
+        patch.status["current_stepCount"]=stepCountData.get("value","Not Enabled")
+        patch.status["stepCount_lastupdated_timestamp"]=str(current_timestamp)
+        prometheus_metrics.stepCount.set(stepCountData.get("value",0))
+    elif not stepCountSpec:
+        patch.status["current_stepCount"]="Not Enabled"
+        patch.status["stepCount_lastupdated_timestamp"]="Not Enabled"
+
+    if activeEnergySpec:
+        activeEnergyData=metrics_collector.collect_metrics_after_interval("activeEnergy",spec["interval"])
+        patch.status["current_activeEnergy"]=activeEnergyData.get("value","Not Enabled")
+        patch.status["activeEnergy_lastupdated_timestamp"]=str(current_timestamp)
+        prometheus_metrics.activeEnergy.set(activeEnergyData.get("value",0))
+    elif not activeEnergySpec:
+        patch.status["current_activeEnergy"]="Not Enabled"
+        patch.status["activeEnergy_lastupdated_timestamp"]="Not Enabled"
+
+    if restingEnergySpec:
+        restingEnergyData=metrics_collector.collect_metrics_after_interval("restingEnergy",spec["interval"])
+        patch.status["current_restingEnergy"]=restingEnergyData.get("value","Not Enabled")
+        patch.status["restingEnergy_lastupdated_timestamp"]=str(current_timestamp)
+        prometheus_metrics.restingEnergy.set(restingEnergyData.get("value",0))
+    elif not restingEnergySpec:
+        patch.status["current_restingEnergy"]="Not Enabled"
+        patch.status["restingEnergy_lastupdated_timestamp"]="Not Enabled"
 
